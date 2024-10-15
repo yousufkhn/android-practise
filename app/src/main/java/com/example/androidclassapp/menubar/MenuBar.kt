3fa105d99a8ef8d1956ad75ef4ae765b5b22dcbf
Muhammad Yousuf Khan <khanyousuf2144@gmail.com>
2024-10-15 13:51:57 +0530

    action bar

--- app/src/main/java/com/example/androidclassapp/menubar/MenuBar.kt
+++ app/src/main/java/com/example/androidclassapp/menubar/MenuBar.kt
@@ -1,6 +1,9 @@
 package com.example.androidclassapp.menubar
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,8 +13,29 @@ import com.example.androidclassapp.R
 class MenuBar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContentView(R.layout.activity_menu_bar)
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.action_settings -> {
+                Toast.makeText(this,"you clicked on settings",Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.action_search ->{
+                Toast.makeText(this,"you cliked on search",Toast.LENGTH_LONG).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
